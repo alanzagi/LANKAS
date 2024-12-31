3ec811fb533f5255765ec08708487851e259a9f8
Alan Zagi <alanzagi2007@gmail.com>
2024-12-31 13:44:05 +0700

    membuat footer menjadi dinamis

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <footer
     class="relative bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6">
     <span class="text-sm text-gray-500 sm:text-center font-bold sm:pl-64">
-        © 2024 By
+        © {{ now()->year }} By
         <a href="https://instagram.com/alanzagi" target="_blank" class="hover:underline hover:text-blue-500">
             Alan "Zagi"
         </a>
